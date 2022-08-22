f003a6afc12a765e35339552b84e709b5de9d76f
Jake Edgar <jakeedgar1012@gmail.com>
2022-08-22 01:31:23 -0700

    update file structure

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Nand } from './types';
+import { Head } from './pure';
+
+let arr: Array<number> = [2, 4, 6];
+
+console.log(Nand(2 + 2 === 5, 2 + 2 === 6));
+
+console.log(Head(arr));
--- src/types/index.js
+++ src/types/index.js
@@ -1,7 +1,16 @@
 "use strict";
 exports.__esModule = true;
-exports.Nand = void 0;
+exports.Nand2 = exports.Nand = void 0;
 var Nand = function (A, B) {
-    A === false && B === false ? true : false;
+    if (A === false && B === false) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 exports.Nand = Nand;
+var Nand2 = function (A, B) {
+    A === false && B === false ? true : false;
+};
+exports.Nand2 = Nand2;
